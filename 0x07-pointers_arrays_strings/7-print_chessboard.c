58f4f345301f851c3de2b4eff98983016d9e4bd1
Kaleabmoges <Kaleabmoges21@gmail.com>
2022-09-27 04:02:47 -0700

    betty fix on task 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,14 +2,14 @@
 
 /**
  * print_chessboard - prints the chessboard
- * @a: pointer to pieces to print 
+ * @a: pointer to pieces to print
  *
  * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
